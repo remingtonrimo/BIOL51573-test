66ee81d9b26e0a4994a7e8a23409ca7a51b89c56
Remington Motte <remingtonribailmotte@gmail.com>
2026-03-31 10:56:39 -0500

    edits to write_slurm

--- scripts/write_slurm.py
+++ scripts/write_slurm.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 
-# we are going to first prompt the user to input the job name
-jobname = input("enter the job name: ")
-time=input("How much time do you need:")
-qname=input("Which queue do you want to use:")
-email=input("Enter your email address:")
-filename=input("Define your filename:")
+import argparse
 
-import sys
-sys.stdout = open(filename, 'w')
+parse_master5000=argparse.ArgumentParser(description="This script creates a slurm script for a job submission")
+
+parse_master5000.add_argument("jobname",help="The name of the job")
+parse_master5000.add_argument("time",help="The amount of time you need")
+parse_master5000.add_argument("qname",help="The queue you want to use")
+parse_master5000.add_argument("email",help="Your email address")
+
+parse_master5000.add_argument("-n","--nodes",help="The number of nodes you want to use",type=int,default=1)
+
+args=parse_master5000.parse_args()
+
+# we are going to first prompt the user to input the job name
 print("#!/bin/bash")
 print()
-print(f"#SBATCH --job-name={jobname}")
-print(f"#SBATCH --partition={qname}")
+print(f"#SBATCH --job-name={args.jobname}")
+print(f"#SBATCH --partition={args.qname}")
+if args.nodes > 1:
+    print(f"#SBATCH --nodes={args.nodes}")
 print(f"#SBATCH --nodes=1")
 print(f"#SBATCH --qos=comp")
 print(f"#SBATCH --tasks-per-node=32")
-print(f"#SBATCH --time={time}")
+print(f"#SBATCH --time={args.time}")
 print(f"#SBATCH -o test_%j.out")
 print(f"#SBATCH -e test_%j.err")
 print(f"#SBATCH --mail-type=ALL")
-print(f"#SBATCH --mail-user={email}")
+print(f"#SBATCH --mail-user={args.email}")
 print()
 print(f"export OMP_NUM_THREADS=32")
 print()
@@ -28,4 +35,3 @@ print(f"module purge")
 print(f"module load intel/18.0.1 impi/18.0.1 mkl/18.0.1")
 print()
 print(f"cd $SLURM_SUBMIT_DIR")
-sys.stdout.close()
