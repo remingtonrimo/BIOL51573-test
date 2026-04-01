d0959fc0e51ac9535aa1b210f825fc2d0ab25264
Remington Motte <remingtonribailmotte@gmail.com>
2026-04-01 10:38:19 -0500

    all the functions work

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -7,12 +7,15 @@ def lawyer():
     # we need an argument parser
     parse_master5000=argparse.ArgumentParser(description="This script calculates the Fibonacci number for a \
                                         given position")
+    
     # command line arguments - required - positional arguments
     parse_master5000.add_argument("position",help="Position in the Fibonacci sequence",type=int)
+    
     # here's an optional argument
     parse_master5000.add_argument("-v","--verbose",help="Print verbose output",action="store_true")
-    # parse the arguments
-    args=parse_master5000.parse_args()
+    
+    # parse the arguments and return them
+    return parse_master5000.parse_args() # one step
 
 # ===== function to calculate the Fibonacci number =====
 def fib_master5000():
@@ -21,20 +24,23 @@ def fib_master5000():
     for i in range(args.position):
         a,b=b,a+b
     fibonacci_number = a
+    return fibonacci_number # two steps
 
 # ===== function to print the output =====
-def print_output():
+def print_output(x):
     if args.verbose:
-        print(f"The Fibonacci number for {args.position} is {fibonacci_number}")
+        print(f"The Fibonacci number for {args.position} is {x}")
     else:
-        print(fibonacci_number)
+        print(x)
 
 
 # ===== main function =====
 def main():
-    lawyer()
-    fib_master5000()
-    print_output()
+    answer = fib_master5000()
+    print_output(answer)
+
+# ===== using lawyer function =====
+args = lawyer()
 
 # set the environment for this script
 # is this master (i.e., a standalone Python script), or
