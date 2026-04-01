20a8ca35b596717f700fcae36bbff2e317e91e31
Remington Motte <remingtonribailmotte@gmail.com>
2026-04-01 10:11:21 -0500

    developing functions

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -2,26 +2,42 @@
 
 import argparse
 
-# we need an argument parser
-parse_master5000=argparse.ArgumentParser(description="This script calculates the Fibonacci number for a \
-                                     given position")
-# command line arguments - required - positional arguments
-parse_master5000.add_argument("position",help="Position in the Fibonacci sequence",type=int)
-# here's an optional argument
-parse_master5000.add_argument("-v","--verbose",help="Print verbose output",action="store_true")
+# ===== function to parse the command line arguments =====
+def lawyer():
+    # we need an argument parser
+    parse_master5000=argparse.ArgumentParser(description="This script calculates the Fibonacci number for a \
+                                        given position")
+    # command line arguments - required - positional arguments
+    parse_master5000.add_argument("position",help="Position in the Fibonacci sequence",type=int)
+    # here's an optional argument
+    parse_master5000.add_argument("-v","--verbose",help="Print verbose output",action="store_true")
+    # parse the arguments
+    args=parse_master5000.parse_args()
 
-# parse the arguments
-args=parse_master5000.parse_args()
+# ===== function to calculate the Fibonacci number =====
+def fib_master5000():
+    # initialise 2 integers
+    a,b=0,1
+    for i in range(args.position):
+        a,b=b,a+b
+    fibonacci_number = a
 
-# initialise 2 integers
-a,b=0,1
+# ===== function to print the output =====
+def print_output():
+    if args.verbose:
+        print(f"The Fibonacci number for {args.position} is {fibonacci_number}")
+    else:
+        print(fibonacci_number)
 
-for i in range(args.position):
-    a,b=b,a+b
 
-fibonacci_number = a
+# ===== main function =====
+def main():
+    lawyer()
+    fib_master5000()
+    print_output()
 
-if args.verbose:
-    print(f"The Fibonacci number for {args.position} is {fibonacci_number}")
-else:
-    print(fibonacci_number)
+# set the environment for this script
+# is this master (i.e., a standalone Python script), or
+# is this a python module
+if __name__ == "__main__":
+    main()
