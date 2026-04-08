b36138900534799662d12f64a8a3085b06f7836f
Remington Motte <remingtonribailmotte@gmail.com>
2026-04-08 10:55:56 -0500

    reading, writing and dictionaries

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-# prompt the user to input their name
-name = input("enter your name: ")
+import argparse
+import cowsay
+
+def lawyer():
+    parse_master5000=argparse.ArgumentParser(description="The cow in this script will greet you")
+
+    parse_master5000.add_argument("name",help="The name of the person to greet")
+    return parse_master5000.parse_args()
 
 # say hello world
-print('Hello world')
+def helloworld():
+    greeting = cowsay.cow("Hello, " + args.name)
+    return greeting
 
-# print greeting
-print('hello,', name)
+def main():
+    greet = helloworld()
+    print(greet)
 
-# some math
-print((7+7)*9)
-print(7+7*9)
+args = lawyer()
+
+if __name__ == "__main__":
+    main()
